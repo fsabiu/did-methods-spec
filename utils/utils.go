51f6d2be1e62b403aac82b35cc9e7ad8e0a6e7d2
ntefa <andrea.batino94@gmail.com>
2022-10-13 15:13:16 +0000

    Created main to split the TxID and random index generator function

---
+++ utils/utils.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"math/rand"
+)
+
+func main() {
+
+	TxID := "bc5221c648533646877505288fc50b6c6100394213694bf111f7a3183074a329"
+	windowLen := 4
+	windowNum := len(TxID) / windowLen
+	startIdx := 0
+	endIdx := windowLen
+	currentChunk := ""
+	for i := 0; i <= windowNum-1; i++ {
+
+		currentChunk = TxID[startIdx:endIdx]
+		fmt.Println(currentChunk)
+		startIdx = startIdx + windowLen
+		endIdx = endIdx + windowLen
+
+	}
+}
+
+func indexGenerator(s string) int {
+
+	h := md5.New()
+	TxID := "bc5221c648533646877505288fc50b6c6100394213694bf111f7a3183074a329"
+	io.WriteString(h, TxID)
+	var seed uint64 = binary.BigEndian.Uint64(h.Sum(nil))
+	rand.Seed(int64(seed))
+	index = rand.Intn(32)
+}
