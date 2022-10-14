9981aa4dbe0f671800b62a4a26cb6f6190f1a90e
ntefa <andrea.batino94@gmail.com>
2022-10-14 13:23:38 +0000

    Added comments to code

--- utils/utils.go
+++ utils/utils.go
@@ -8,28 +8,33 @@ import (
 	"math/rand"
 )
 
+const encodeStd = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+const windowLen = 4
+
 func main() {
 
 	TxID := "bc5221c648533646877505288fc50b6c6100394213694bf111f7a3183074a329"
 
-	idString := GenerateIdString(TxID, 4)
+	idString := GenerateIdString(TxID, windowLen)
 	fmt.Println("Idstring : ", idString)
 
 }
 
+//IndexGenerator takes as input a string generates an integer in the interval [0,31]
+//The string is hashed into an integer and used as seed of the random generation
 func indexGenerator(s string) int {
 
-	h := md5.New()
-	io.WriteString(h, s)
-	var seed uint64 = binary.BigEndian.Uint64(h.Sum(nil))
-	rand.Seed(int64(seed))
+	h := md5.New()                              //init new new hash.Hash object computing the MD5 checksum
+	io.WriteString(h, s)                        //write string into object
+	seed := binary.BigEndian.Uint64(h.Sum(nil)) //compute checksum and
+	rand.Seed(int64(seed))                      //set seed
 	index := rand.Intn(32)
 	return index
 }
 
+//EncodeStringBase32 takes as input a string of arbitrary length and maps it into a base32 char
 func EncodeStringBase32(s string) string {
 	index := indexGenerator(s)
-	const encodeStd = "abcdefghijklmnopqrstuvwxyz234567"
 	base32Encoded := encodeStd[index : index+1]
 	return base32Encoded
 }
@@ -44,13 +49,12 @@ func GenerateIdString(TxID string, windowLen int) string {
 
 	for i := 0; i <= windowNum-1; i++ {
 
-		currentChunk = TxID[startIdx:endIdx]
-		//fmt.Println(currentChunk)
+		currentChunk = TxID[startIdx:endIdx] //get current 4 chars chunk of TxId
 		startIdx = startIdx + windowLen
 		endIdx = endIdx + windowLen
-		base32Encoded := EncodeStringBase32(currentChunk)
-		idString = idString + base32Encoded
-
+		idChar := EncodeStringBase32(currentChunk)
+		idString = idString + idChar //append new base32 char to idString
 	}
+
 	return idString
 }
